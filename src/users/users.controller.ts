872b1c26b32baa9682fb979ac8f73f0bef63301c
riya <riyathreya@gmail.com>
2024-01-13 21:31:39 +0530

    feat: added CRUD over user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,48 @@
-import { Controller, Body } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Get,
+  Param,
+  Query,
+  Delete,
+  Patch,
+} from '@nestjs/common';
 import { Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   /**
-   * Craete a new user
+   * Create a new user
    * @param body
    * @returns
    */
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     return this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get('/')
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -10,5 +10,58 @@ export class UsersService {
   create(email: string, password: string) {
     const user = this.repo.create({ email, password });
     this.repo.save(user);
+  }
+
+  /**
+   * Finds one record that matches given criteria
+   * @param id
+   * @returns
+   */
+  async findOne(id: number) {
+    const user = await this.repo.findOneBy({ id }); // {email} whatever criteria
+    if (!user) {
+      throw new NotFoundException('User not found!');
+    }
+    return user;
+  }
+
+  /**
+   * Return users basis email
+   * @param email
+   * @returns
+   */
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  /**
+   * Update user
+   * @param id
+   * @param attrs
+   * @returns
+   */
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('User not found!');
+    }
+
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  /**
+   * Delete user
+   * @param id
+   * @returns
+   */
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('User not found!');
+    }
+
+    // this.repo.delete(id); delete does not internally call hooks. so using remove by passing user entity
+    return this.repo.remove(user);
   }
 }
